d31b4367237bc52b067c66e72262ab1223cef720
Vitaliy <theyamshikov@yandex.ru>
2018-09-09 23:16:49 +0600

    refacto

--- guide/ru/README.md
+++ guide/ru/README.md
@@ -7,8 +7,4 @@
 * [Конфигурация](config.md)
 * [Руководство](guide.md)
 
-## Паттерны
 
-* [Команда](pattern-command.md)
-* [Фильтр](pattern-filter.md)
-* [Выключение фильтров и команд](pattern-is-enabled.md)
--- src/scenario/base/BaseScenario.php
+++ src/scenario/base/BaseScenario.php
@@ -2,29 +2,12 @@
 
 namespace yii2lab\designPattern\scenario\base;
 
-use yii\base\BaseObject;
+/**
+ * Class BaseScenario
+ * @package yii2lab\designPattern\scenario\base
+ *
+ * @deprecated
+ */
+abstract class BaseScenario extends \yii2lab\extension\scenario\base\BaseScenario {
 
-abstract class BaseScenario extends BaseObject {
-	
-	private $data;
-	public $isEnabled = true;
-	
-	abstract public function run();
-	
-	public function isEnabled() {
-		return $this->isEnabled;
-	}
-	
-	public function setData($value) {
-		$this->data = $value;
-	}
-	
-	public function issetData() {
-		return isset($this->data);
-	}
-	
-	public function getData() {
-		return $this->data;
-	}
-	
 }
